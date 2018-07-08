e5a99dc7d335a3fd826e2d54075d9a8e1cc69fe3
ikom_ <fortiko1229@gmail.com>
2018-07-08 19:26:39 +0900

    Won/lost message modified

--- dicegame.py
+++ dicegame.py
@@ -6,6 +6,6 @@ d1=random.randint(1,6)
 d2=random.randint(1,6)
 print('Rolling the dice...\nDie 1: ',d1,'\nDie 2: ',d2,'\nTotal value: ',d1+d2)
 if d1+d2>7:
-    print('You won.')
+    print(x,'won!')
 else :
-    print('You lost')
+    print(x,'lost.')
