b2cd613572bfa70097aca7e88d675d403a01daf6
ikom_ <fortiko1229@gmail.com>
2018-07-08 19:12:12 +0900

    Greeting added

--- dicegame.py
+++ dicegame.py
@@ -1,4 +1,6 @@
 import random
+print('What is your name?')
+print('Hello,',input('>'),'!')
 d1=random.randint(1,6)
 d2=random.randint(1,6)
 print('Rolling the dice...\nDie 1: ',d1,'\nDie 2: ',d2,'\nTotal value: ',d1+d2)
